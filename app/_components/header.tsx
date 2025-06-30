08388538ec8bf1c8a20144d242ca2c192c88ed20
Fabricio9227 <fabriciomarthinsdasilva@gmail.com>
2025-06-30 17:54:46 -0300

    fix: rm unused components and fix next.js errors

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,27 +1,17 @@
-import Image from "next/image"
-import { Card, CardContent } from "./ui/card"
-import { Button } from "./ui/button"
+"use client"
 import { MenuIcon } from "lucide-react"
-import { Sheet, SheetTrigger } from "./ui/sheet"
-import SidebarSheet from "./sidebar-sheet"
-import Link from "next/link"
+import { Button } from "./ui/button"
+import { Card, CardContent } from "./ui/card"
+import Image from "next/image"
 
 const Header = () => {
   return (
     <Card>
-      <CardContent className="flex flex-row items-center justify-between p-5">
-        <Link href="/">
-          <Image alt="FSW Barber" src="/logo.png" height={18} width={120} />
-        </Link>
-
-        <Sheet>
-          <SheetTrigger asChild>
-            <Button size="icon" variant="outline">
-              <MenuIcon />
-            </Button>
-          </SheetTrigger>
-          <SidebarSheet />
-        </Sheet>
+      <CardContent className="flex flex-col items-center justify-center space-y-4">
+        <Image src="/logo.png" alt="FSW Barber" height={18} width={120} />
+        <Button size="icon" variant="outline">
+          <MenuIcon />
+        </Button>
       </CardContent>
     </Card>
   )
---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import Header from "./_components/header"
+
+const Home = () => {
+  return (
+    <div>
+      {/* Header*/}
+      <Header />
+      <div className="p-5"></div>
+    </div>
+  )
+}
+export default Home
