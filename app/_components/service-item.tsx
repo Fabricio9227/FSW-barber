fe7e4dd9af516be0b3194cdbe0ab01ddd7cc8fd0
Fabricio9227 <fabriciomarthinsdasilva@gmail.com>
2025-07-23 12:28:50 -0300

    feat: ensure onlu users can book services

--- app/_components/service-item.tsx
+++ app/_components/service-item.tsx
@@ -54,6 +54,7 @@ const TIME_LIST = [
 ]
 
 const getTimeList = (bookings: Booking[]) => {
+  // TODO: Não exibir horrários no passado
   return TIME_LIST.filter((time) => {
     const hour = Number(time.split(":")[0])
     const minute = Number(time.split(":")[1])
@@ -125,7 +126,6 @@ const ServiceItem = ({ service, barbershop }: ServiceItemProps) => {
 
       await createBooking({
         serviceId: service.id,
-        userId: (data?.user as any).id,
         date: newDate,
       })
       toast.success("Reserva criada com sucesso!")
